3f5c926f50bfa00213c5136295370df68b445816
Hunter1 <info@web-hunters.com>
2019-04-05 08:37:16 +0300

    start pug

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var browserSync = require('browser-sync').create();
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 var spritesmith = require('gulp.spritesmith');
+var rimraf = require('rimraf');
+var rename = require('gulp-rename');
 
 
 // Static server
@@ -19,7 +21,7 @@ gulp.task('server', function() {
 
 
 // PUG
-gulp.task('compile', function buildHTML() {
+gulp.task('compile:pug', function buildHTML() {
     return gulp.src('dev/template/index.pug')
     .pipe(pug({
       pretty: true
@@ -29,9 +31,63 @@ gulp.task('compile', function buildHTML() {
 
 
 // SASS
-gulp.task('sass', function () {
+gulp.task('compile:sass', function () {
     return gulp.src('dev/styles/main.scss')
-      .pipe(sass().on('error', sass.logError))
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename('main.min.css'))
       .pipe(gulp.dest('distr/css'));
 });
+
+
+// SPRITE
+gulp.task('sprite', function (cb) {
+    var spriteData = gulp.src('dev/images/icons/*.png').pipe(spritesmith({
+      imgName: 'sprite.png',
+      imgPath: '../images/sprite.png',
+      cssName: 'sprite.scss'
+    }));
+
+    spriteData.img.pipe(gulp.dest('distr/images'));
+    spriteData.css.pipe(gulp.dest('dev/styles/global/'));
+    cb();
+});
+
+
+// RIMRAF (delete)
+gulp.task('clean', function del(cb) {
+    return rimraf('distr',cb);
+});
+
+
+// COPY FONTS
+gulp.task('copy:fonts',function() {
+    return gulp.src('./dev/fonts/**/*')
+    .pipe(gulp.dest('distr/fonts'));
+});
+
+
+// COPY IMAGES
+gulp.task('copy:images', function() {
+    return gulp.src('./dev/images/**/*')
+    .pipe(gulp.dest('distr/images'));
+});
+
+
+// COPY
+gulp.task('copy', gulp.parallel('copy:fonts','copy:images'));
+
+
+// WATCHERS
+gulp.task('watch', function() {
+    gulp.watch('dev/template/**/*.pug', gulp.series('compile:pug'));
+    gulp.watch('dev/styles/**/*.scss', gulp.series('compile:sass'));
+})
+
+
+gulp.task('default', gulp.series(
+    'clean',
+    gulp.parallel('compile:pug','compile:sass','sprite','copy'),
+    gulp.parallel('watch','server')
+))
+
    
